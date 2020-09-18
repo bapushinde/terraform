b96ed094b1fe0ff28f765f4e430e0c493074f939
bapushinde <bapushinde@gmail.com>
2020-09-19 06:52:01 +0800

    oops hard code values..

--- module-demo/backend.tf
+++ module-demo/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket      = "practice-terraform-dev"
-    prefix      = "state-19-sep"
-    credentials = "~/practice/Terraform/molten-raceway-282113-a0de69550adb.json"
+    bucket      = "<bucket>"
+    prefix      = "<key_prefix>"
+    credentials = "~/practice/Terraform/<service_account>.json"
   }
 }
