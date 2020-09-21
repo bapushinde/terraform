88fcea28576ed41eb1c387308289b670cb297388
bapushinde <bapushinde@gmail.com>
2020-09-22 05:36:56 +0800

    use variables

--- gcs-example/main.tf
+++ gcs-example/main.tf
@@ -8,8 +8,8 @@ terraform {
 
 provider "google" {
   version     = "3.5.0"
-  credentials = file("~/<DIR_PATH>/<Service_Account_File_Name>.json")
-  project     = "<Project_ID>"
+  credentials = var.serviceaccount
+  project     = "molten-raceway-282113"
   region      = "us-east1"
   zone        = "us-east1-b"
 }
